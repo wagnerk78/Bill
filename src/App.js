beec1cdf3c87e58d8014596a6c15acd0aac55ad6
wagnerk78 <wagnerkroiss@gmail.com>
2022-10-06 20:11:13 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,158 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react"
+import { Container, Div1, Div2, Div3, Div4, Div5, Div6, Div7, Div8, Label, Img, Img2, Img3, Button, Button2, Input, Price, InputBill, Label2, Label4, InputPeople, Label3 } from './styles'
+import logo from "./images/logo.svg"
+import cifrao from "./images/icon-dollar.svg"
+import people from "./images/icon-person.svg"
 
 function App() {
+  const [num1, setNum1] = useState(0)
+  const [num2, setNum2] = useState(0)
+  const [num3, setNum3] = useState(0)
+  const [resultado, setResultado] = useState(0)
+  const [tip, setTip] = useState(0)
+  const [color, setColor] = useState(true)
+
+
+  const calcular = () => {
+    const valor = (parseFloat(num1) * 1.05) / parseFloat(num2)
+    var valor1 = parseFloat(valor.toFixed(2))
+    setResultado(valor1)
+    const tip = (parseFloat(num1) * 0.05) / parseFloat(num2)
+    var valor2 = parseFloat(tip.toFixed(2))
+    setTip(valor2)
+    if (color === true)
+      setColor(true)
+  }
+
+  const calcular2 = () => {
+    const valor = (parseFloat(num1) * 1.1) / parseFloat(num2)
+    var valor1 = parseFloat(valor.toFixed(2))
+    setResultado(valor1)
+    const tip = (parseFloat(num1) * 0.10) / parseFloat(num2)
+    var valor2 = parseFloat(tip.toFixed(2))
+    setTip(valor2)
+    if (color === true)
+      setColor(false)
+  }
+
+  const calcular3 = () => {
+    const valor = (parseFloat(num1) * 1.15) / parseFloat(num2)
+    var valor1 = parseFloat(valor.toFixed(2))
+    setResultado(valor1)
+    const tip = (parseFloat(num1) * 0.15) / parseFloat(num2)
+    var valor2 = parseFloat(tip.toFixed(2))
+    setTip(valor2)
+    if (color === true)
+      setColor(false)
+  }
+
+
+  const calcular4 = () => {
+    const valor = (parseFloat(num1) * 1.25) / parseFloat(num2)
+    var valor1 = parseFloat(valor.toFixed(2))
+    setResultado(valor1)
+    const tip = (parseFloat(num1) * 0.25) / parseFloat(num2)
+    var valor2 = parseFloat(tip.toFixed(2))
+    setTip(valor2)
+    if (color === true)
+      setColor(false)
+  }
+
+  const calcular5= () => {
+    const valor = (parseFloat(num1) * 1.5) / parseFloat(num2)
+    var valor1 = parseFloat(valor.toFixed(2))
+    setResultado(valor1)
+    const tip = (parseFloat(num1) * 0.5) / parseFloat(num2)
+    var valor2 = parseFloat(tip.toFixed(2))
+    setTip(valor2)
+    if (color === true)
+      setColor(false)
+  }
+  const calcular6= () => {
+    const percentual = parseFloat(num3/100)
+    const valor = (parseFloat(num1)*(1+percentual)) / parseFloat(num2)
+    var valor1 = parseFloat(valor.toFixed(2))
+    setResultado(valor1)
+    const tip = (parseFloat(num1) * (num3/100)) / parseFloat(num2)
+    var valor2 = parseFloat(tip.toFixed(2))
+    setTip(valor2)
+   
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Div1>
+        <Img src={logo} alt="logo" />
+      </Div1>
+      <Div2>
+        <Div3>
+          <Label>Bill</Label>
+          <Div7>
+            <Img2 src={cifrao} alt="money" />
+            <InputBill
+              value={num1}
+              onChange={(e) => setNum1(e.target.value)}
+            />
+          </Div7>
+          <Label>Select Tip %</Label>
+
+          <Button
+            onClick={() => { calcular() }}
+            isColor={color}
+          >5% </Button>
+          <Button
+            onClick={() => { calcular2() }}
+            isColor={color}
+          >10%</Button>
+          <Button
+            onClick={() => { calcular3() }}
+            isColor={color}
+          >15%</Button>
+          <Button
+           onClick={() => { calcular4() }}
+           isColor={color}        
+          >25%</Button>
+          <Button
+           onClick={() => { calcular5() }}
+           isColor={color}
+          >50%</Button>
+          <Input
+          type='number'
+           value={num3}
+           onChange={(e) => setNum3(e.target.value)}
+           onClick={() => { calcular6() }}
+          ></Input>
+          <Label2> Number of People</Label2>
+          <Div8>
+            <Img3 src={people} alt="person" />
+            <InputPeople
+              value={num2}
+              onChange={(e) => setNum2(e.target.value)}
+            />
+          </Div8>
+        </Div3>
+        <Div4>
+          <Div5>
+            <div>
+              <div><Label3> Tip Amount </Label3></div>
+              <div><Label4> / person </Label4></div>
+            </div>
+            <Price> $ {tip} </Price>
+          </Div5>
+          <Div6>
+            <div>
+              <div><Label3>  Total </Label3></div>
+              <div><Label4> / person </Label4></div>
+            </div>
+            <Price> $ {resultado} </Price>
+          </Div6>
+          <Button2
+            type="reset"
+            >
+            RESET</Button2>
+        </Div4>
+      </Div2>
+    </Container>
   );
 }
 
---
+++ src/styles.js
@@ -0,0 +1,158 @@
+import styled from 'styled-components'
+
+
+export const Container = styled.div`
+background: #c5e4e7;
+width: 100vw;
+height: 100vh;
+display: grid;
+align-content: center;
+justify-content: center;
+`
+export const Div1 = styled.div`
+margin-left: 275px;
+margin-bottom: 70px;
+`
+
+export const Div2 = styled.div`
+background-color: white;
+width: 600px;
+height: 300px;
+padding: 20px;
+border-radius: 20px;
+display: flex;
+align-items: center;
+
+`
+
+export const Img = styled.img`
+width: 50px;
+`
+export const Img2 = styled.img`
+width: 12px;
+height: 12px;
+margin-top: 7px;
+margin-left: 10px;
+`
+export const Img3 = styled.img`
+width: 12px;
+height: 12px;
+margin-top: 7px;
+margin-left: 10px;
+`
+
+export const Label = styled.h1`
+font-size: 12px;
+color: #5e7a7d;
+`
+
+export const Label2 = styled.h1`
+font-size: 12px;
+color: #5e7a7d;
+margin-top: 50px;
+`
+export const Label3 = styled.h2`
+font-size: 10px;
+color: white;
+width: 70px;
+margin-left: 20px;
+margin-top: 30px;
+
+`
+export const Label4 = styled.h2`
+font-size: 10px;
+color: #5e7a7d;
+width: 70px;
+margin-left: 20px;
+`
+
+
+
+export const Div3 = styled.div`
+width: 300px;
+height: 300px;
+`
+
+export const Div4 = styled.div`
+background: #00494d;
+width: 300px;
+height: 300px;
+border-radius: 20px;
+`
+export const Button = styled.button`
+background-color: ${props => props.color ? ' #26c0ab' :'#00494d' };
+color: white;
+border: none;
+width: 70px;
+height: 25px;
+margin:  8px 10px 0px 10px;
+border-radius: 5px;
+`
+
+export const Input = styled.input`
+width: 60px;
+height: 25px;
+margin-left: 10px;
+background: #f4fafa;
+border: none;
+outline: 0;
+`
+export const InputBill = styled.input`
+width: 250px;
+height: 25px;
+border: none;
+background: #f4fafa;
+margin-bottom: 25px;
+text-align: right;
+outline: 0;
+`
+
+export const InputPeople = styled.input`
+width: 250px;
+height: 25px;
+border: none;
+background: #f4fafa;
+text-align: right;
+outline: 0;
+`
+
+export const Div5 = styled.div`
+display: flex;
+justify-content: space-between;
+`
+
+export const Div6 = styled.div`
+display: flex;
+justify-content: space-between;
+`
+export const Div7 = styled.div`
+display: flex;
+background: #f4fafa;
+width: 250px;
+height: 25px;
+`
+export const Div8 = styled.div`
+display: flex;
+background: #f4fafa;
+width: 250px;
+height: 25px;
+`
+
+export const Price = styled.div`
+color: #26c0ab;
+font-size: 25px;
+margin-top: 30px;
+margin-right: 20px;
+`
+
+export const Button2 = styled.button`
+width: 200px;
+height: 30px;
+background: #26c0ab;
+color: #00494d;
+border: none;
+border-radius: 2px;
+margin-top: 90px;
+margin-left: 50px;
+
+`
